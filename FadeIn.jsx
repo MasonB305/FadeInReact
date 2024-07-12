4e3d4757b50cca7f9c8e0f9adc0d7f82f22c0eb6
MasonB305 <100322451+MasonB305@users.noreply.github.com>
2024-07-12 17:39:10 -0500

    more smooth transition

--- FadeIn.jsx
+++ FadeIn.jsx
@@ -6,11 +6,11 @@ return (
     <FadeIn Component={INSERTCOMPONENTNAMEHERE} />
 </>
 )
-    */
+*/
 function FadeIn({ Component }) {
-    const [opacity, setOpacity] = useState(0);
+    const [opacity, setOpacity] = useState(.1);
     useEffect(() => {
-        const intervalId = setInterval(() => {
+        /*const intervalId = setInterval(() => {
             setOpacity((a) => Math.min(1, a + .03))
         }, 100);
 
@@ -20,6 +20,25 @@ function FadeIn({ Component }) {
         return () => {
             clearInterval(intervalId);
             clearTimeout(timeoutId);
+        };
+    }, []);
+    */
+        const clickHandler = () => {
+            setOpacity(1);
+        }
+        document.addEventListener("click", clickHandler);
+
+        const intervalId = setInterval(() => {
+            setOpacity((a) => Math.min(1, a * 1.1))
+        }, 100);
+        const timeoutId = setTimeout(() => {
+            clearInterval(intervalId);
+            document.removeEventListener("click", clickHandler);
+        }, 1000 / .3 + 500);
+        return () => {
+            clearInterval(intervalId);
+            clearTimeout(timeoutId);
+            document.removeEventListener("click", clickHandler);
         };
     }, []);
     return (
